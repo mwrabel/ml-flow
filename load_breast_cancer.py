b28108670170b0c434c120446f556c738f6a2ac3
Michal <michal.wrabel7@gmail.com>
2021-02-25 12:26:46 +0100

    fix halving random search cv

--- load_breast_cancer.py
+++ load_breast_cancer.py
@@ -14,20 +14,22 @@ import numpy as np
 from scipy.stats import randint, uniform
 from sklearn.metrics import roc_auc_score
 
+from sklearn.datasets import fetch_kddcup99
 # Load data
-# df = load_breast_cancer(return_X_y=True, as_frame=True)
-# x = df[0]
-# y = df[1]
-# del df
+df = load_breast_cancer(return_X_y=True, as_frame=True)
+x = df[0]
+y = df[1]
+del df
 
-x, y = make_classification(n_samples=5000, n_features=100, n_informative=10, n_redundant=10, n_repeated=5, flip_y=0.01, n_classes=2, class_sep=0.6, random_state=10)
+# x, y = make_classification(n_samples=5000, n_features=30, n_informative=10, n_redundant=5, n_repeated=5, flip_y=0.1, n_classes=2, class_sep=0.01, random_state=10)
+
 
 
 # Split to train / test
 train_features, test_features, train_labels, test_labels = train_test_split(x, y, test_size=0.2, random_state=42)
 
 # Fit out of the box RandomForestClassifier
-rf = RandomForestClassifier()
+rf = RandomForestClassifier(n_estimators=10)
 rf.fit(train_features, train_labels)
 # Score model
 score_rf = roc_auc_score(test_labels, rf.predict_proba(test_features)[:, 1])
@@ -41,13 +43,13 @@ pipe = Pipeline([
 ])
 
 # Build CV folds
-cv = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+cv = StratifiedKFold(n_splits=3, random_state=42, shuffle=True)
 
 # Declare param grid
 param_grid = {
     # 'selector__k': randint(1, x.shape[1]+1),
-    'classifier__n_estimators': randint(50, 500),
-    'classifier__max_samples': uniform(0.1, 0.9)
+    'classifier__n_estimators': randint(10, 500),
+    # 'classifier__max_samples': uniform(0.1, 0.9) # 0.1 -> 0.9
     # 'classifier__criterion': ['gini', 'entropy'],
     # 'classifier__max_depth': randint(10, 20),
     # 'classifier__min_samples_split': randint(2, 4),
@@ -58,7 +60,7 @@ param_grid = {
 }
 
 # Fit rf model with RandomizedSearchCV
-clf = RandomizedSearchCV(pipe, param_grid, cv=cv, verbose=1, n_iter=20, scoring='roc_auc', n_jobs=-1)
+clf = RandomizedSearchCV(pipe, param_grid, cv=cv, verbose=2, n_iter=5, scoring='roc_auc', n_jobs=-1)
 clf = clf.fit(train_features, train_labels)
 # Score model
 score_randomized = roc_auc_score(test_labels, clf.predict_proba(test_features)[:, 1])
@@ -67,8 +69,8 @@ print(clf.best_params_)
 
 # Fit rf model with HalvingRandomSearchCV
 clf_halving = HalvingRandomSearchCV(pipe, param_grid, cv=cv, verbose=1, scoring='roc_auc', n_jobs=-1,
-                                    aggressive_elimination=True, factor=3, min_resources=50)
-clf_halving = clf_halving.fit(x, y)
+                                    aggressive_elimination=True, factor=2, min_resources=20)
+clf_halving = clf_halving.fit(train_features, train_labels)
 # Score model
 score_halving = roc_auc_score(test_labels, clf_halving.predict_proba(test_features)[:, 1])
 print(f'ROC AUC Score for HalvingRandomSearchCV model: {score_halving}')
